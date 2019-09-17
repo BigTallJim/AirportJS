c068a24af5494022754000fcd5f0dd3288f74204
James Holton <james_holton@yahoo.co.uk>
2019-09-17 16:34:13 +0100

    Added stuff

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,12 +1,16 @@
 describe('Plane', function() {
     
     beforeEach(function() {
-        airport = new Airport();
+        airport = new Airport(3);
         plane1 = new Plane();
+        plane2 = new Plane();
+        plane3 = new Plane();
+        plane4 = new Plane();
     });
 
 
     it("Lands at an airport", function(){
+        spyOn(airport, "checkWeather").and.returnValue('Sunny');
         airport.landPlane(plane1);
         expect(airport._planes[0]).toEqual(plane1);
     });
@@ -26,5 +30,18 @@ describe('Plane', function() {
     it("Prevents takeoff when weather is stormy", function(){
         spyOn(airport, "checkWeather").and.returnValue('Stormy');
         expect(function(){airport.takeOffPlane(plane1)}).toThrowError('TOO STORMY!');
+    });
+
+    it("Prevent landing when weather is stormy", function(){
+        spyOn(airport, "checkWeather").and.returnValue('Stormy');
+        expect(function(){airport.landPlane(plane1)}).toThrowError('TOO STORMY TO LAND!');
+    });
+
+    it("Prevent landing when airport is full", function(){
+        spyOn(airport, "checkWeather").and.returnValue('Sunny');
+        airport.landPlane(plane1);
+        airport.landPlane(plane2);
+        airport.landPlane(plane3);
+        expect(function(){airport.landPlane(plane4)}).toThrowError('AIRPORT FULL!');
     });
 });
--- src/airport.js
+++ src/airport.js
@@ -1,21 +1,31 @@
-function Airport() {
+function Airport(size) {
     this._planes = [];
-
+    this._maxPlanes = size;
+    
 }
 
 Airport.prototype.landPlane = function(plane){
+    if (this.checkFull() === true){
+        throw new Error('AIRPORT FULL!');
+    };
+
+    if (this.checkWeather(0.5) === "Stormy"){
+        throw new Error('TOO STORMY TO LAND!');
+    };
     this._planes.push(plane);
 }
 
+// Airport.prototype.maxPlanes = function(){
+//     return this.numMaxPlanes;
+// }
+
 Airport.prototype.takeOffPlane = function(plane){
     var index = this._planes.indexOf(plane)
-    // console.log(index);
     if (this.checkWeather(0.5) === "Stormy") {
         throw new Error('TOO STORMY!');
     };
     if (index > -1){
         this._planes.splice(index, 1)
-        console.log("not stormy")
     };
 };
 
@@ -25,4 +35,11 @@ Airport.prototype.checkWeather = function(percentage){
         return "Stormy"
     };
         return "Sunny"
+};
+
+Airport.prototype.checkFull = function(){
+    if (this._planes.length === this._maxPlanes){
+        return true;
+    };
+    return false;
 };
